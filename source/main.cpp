be46c856d1c40a2b5c3e55373bccdeddf143ad20
richelbilderbeek <richel@richelbilderbeek.nl>
2017-10-09 19:57:29 +0200

    Compiles under 32 bit

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,10 @@
 #include <nds.h>
 #include <iostream>
 
+# For 32 bit systems, add this line:
+# https://github.com/devkitPro/buildscripts/issues/26
+extern "C" void __sync_synchronize() {}
+
 int main()
 {
   //Initialization
